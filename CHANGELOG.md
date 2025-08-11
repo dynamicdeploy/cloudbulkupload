fbab3baa1211e5d52c2e4e961deec71f9c4163bd
Tejaswi Redkar <dynamicdeploy@live.com>
2025-08-11 16:46:00 -0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,31 @@
+# Change Log:
+**v1.1.3:**
+- Add examples as a Jupyter Notebook file.
+
+**v1.1.2:**
+- Add Standalone MinIO deployment link to README.md.
+- Add blog posts to README.md.
+
+**v1.1.1:**
+- Fix licence badge.
+
+**v1.1.0:**
+- Change package name to `bulkboto3`
+
+**v1.0.3:**
+- Add use case of transferring arbitrary files to S3
+
+**v1.0.2:**
+- Fix `find_namespace_packages` argument in `setup.py`
+
+**v1.0.1:**
+- Fix links for PyPI
+
+**v1.0.0:**
+- Initial release
+- Features:
+  - Multi-thread uploading/downloading of a directory (keeping the directory structure) to/from S3 object storage
+  - Deleting all objects of an S3 bucket
+  - Checking the existence of an object on the S3 bucket
+  - Listing all objects on an S3 bucket
+  - Creating a new S3 bucket on the object storage
---
+++ examples.py
@@ -0,0 +1,99 @@
+import logging
+
+from bulkboto3 import BulkBoto3, StorageTransferPath
+
+logging.basicConfig(
+    level="INFO",
+    format="%(asctime)s — %(levelname)s — %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
+TARGET_BUCKET = "test-bucket"
+NUM_TRANSFER_THREADS = 50
+TRANSFER_VERBOSITY = True
+
+# instantiate a BulkBoto3 object
+bulkboto_agent = BulkBoto3(
+    resource_type="s3",
+    endpoint_url="<Your storage endpoint>",
+    aws_access_key_id="<Your access key>",
+    aws_secret_access_key="<Your secret key>",
+    max_pool_connections=300,
+    verbose=TRANSFER_VERBOSITY,
+)
+
+# create a new bucket
+bulkboto_agent.create_new_bucket(bucket_name=TARGET_BUCKET)
+
+# upload a whole directory with its structure to an S3 bucket in multi thread mode
+bulkboto_agent.upload_dir_to_storage(
+    bucket_name=TARGET_BUCKET,
+    local_dir="test_dir",
+    storage_dir="my_storage_dir",
+    n_threads=NUM_TRANSFER_THREADS,
+)
+
+# download a whole directory with its structure to a local directory in multi thread mode
+bulkboto_agent.download_dir_from_storage(
+    bucket_name=TARGET_BUCKET,
+    storage_dir="my_storage_dir",
+    local_dir="new_test_dir",
+    n_threads=NUM_TRANSFER_THREADS,
+)
+
+# upload arbitrary files to an S3 bucket
+upload_paths = [
+    StorageTransferPath(
+        local_path="test_dir/first_subdir/f2",
+        storage_path="f2",
+    ),
+    StorageTransferPath(
+        local_path="test_dir/second_subdir/f4",
+        storage_path="my_storage_dir/f4",
+    ),
+]
+bulkboto_agent.upload(bucket_name=TARGET_BUCKET, upload_paths=upload_paths)
+
+# download arbitrary files from an S3 bucket
+download_paths = [
+    StorageTransferPath(
+        storage_path="f2",
+        local_path="f2",
+    ),
+    StorageTransferPath(
+        storage_path="my_storage_dir/f4",
+        local_path="f5",
+    ),
+]
+bulkboto_agent.download(
+    bucket_name=TARGET_BUCKET, download_paths=download_paths
+)
+
+# check if a file exists in a bucket
+print(
+    bulkboto_agent.check_object_exists(
+        bucket_name=TARGET_BUCKET,
+        object_path="my_storage_dir/first_subdir/test_file.txt",
+    )
+)
+print(
+    bulkboto_agent.check_object_exists(
+        bucket_name=TARGET_BUCKET, object_path="my_storage_dir/first_subdir/f1"
+    )
+)
+
+# get list of objects in a bucket (with prefix)
+print(
+    bulkboto_agent.list_objects(
+        bucket_name=TARGET_BUCKET, storage_dir="my_storage_dir"
+    )
+)
+print(
+    bulkboto_agent.list_objects(
+        bucket_name=TARGET_BUCKET, storage_dir="my_storage_dir/second_subdir"
+    )
+)
+
+# delete all objects on a bucket
+bulkboto_agent.empty_bucket(TARGET_BUCKET)
